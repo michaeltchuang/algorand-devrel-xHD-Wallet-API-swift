0fe9b8112e7ba6196a1b99ce1690070e1b54b9b1
Michael T Chuang <github@michaeltchuang.com>
2025-08-26 16:14:07 -0700

    fix: update sodium package

--- Package.swift
+++ Package.swift
@@ -19,11 +19,11 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/a2/MessagePack.swift.git", from: "4.0.0"),
         .package(url: "https://github.com/algorandfoundation/JSONSchema.swift.git", exact: "0.7.0"),
-        .package(url: "https://github.com/algorandfoundation/swift-sodium-full.git", from: "1.0.0"),
+        .package(url: "https://github.com/sinoru/swift-sodium.git", from: "0.0.2"),
         .package(url: "https://github.com/attaswift/BigInt.git", from: "5.2.1"),
         .package(url: "https://github.com/Electric-Coin-Company/MnemonicSwift.git", from: "2.2.4"),
-        .package(url: "https://github.com/realm/SwiftLint", from: "0.55.0"),
-        .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.53.9"),
+        .package(url: "https://github.com/realm/SwiftLint", from: "0.59.1"),
+        .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.57.2"),
         .package(url: "https://github.com/norio-nomura/Base32.git", from: "0.9.0"),
     ],
     targets: [
@@ -33,7 +33,7 @@ let package = Package(
                 .product(name: "BigInt", package: "BigInt"),
                 .product(name: "JSONSchema", package: "JSONSchema.swift"),
                 .product(name: "MessagePack", package: "MessagePack.swift"),
-                .product(name: "Sodium", package: "swift-sodium-full"),
+                .product(name: "Sodium", package: "swift-sodium"),
             ],
             plugins: [
                 .plugin(name: "SwiftLintBuildToolPlugin", package: "SwiftLint"),
